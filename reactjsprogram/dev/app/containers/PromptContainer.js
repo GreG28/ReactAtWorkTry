5b315b782571e5825ad33e2d7a38e19f8c51c5d3
GreG28 <gregoire.fouillard@gmail.com>
2016-04-23 00:33:00 +0200

    correction d'une erreur lors de la fin de la précédente section

--- reactjsprogram/dev/app/containers/PromptContainer.js
+++ reactjsprogram/dev/app/containers/PromptContainer.js
@@ -24,14 +24,14 @@ var PromptContainer = React.createClass({
 
         if (this.props.routeParams.playerOne) {
             this.context.router.push({
-                pathName: '/battle',
+                pathname: '/battle',
                 query: {
                     playerOne : this.props.routeParams.playerOne,
-                    playerOne : this.state.username,
+                    playerTwo : this.state.username,
                 }
-            });
+            })
         } else {
-            this.context.router.push('/playerTwo/' + this.state.username);
+            this.context.router.push('/playerTwo/' + this.state.username)
         }
     },
     render: function() {
