1839261da332e23c133fcd73ee41cb0e725103f7
GreG28 <gregoire.fouillard@gmail.com>
2016-04-23 02:25:50 +0200

    Fin de la partie : Axios, Promises, and the Github API

--- reactjsprogram/dev/app/containers/ConfirmBattleContainer.js
+++ reactjsprogram/dev/app/containers/ConfirmBattleContainer.js
@@ -1,35 +1,33 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes: {
         router: React.PropTypes.object.isRequired
     },
     getInitialState: function() {
-        console.log('getInitialState');
         return {
             isLoading: true,
-            playerInfo: []
+            playersInfo: []
         };
-    },
-    componentWillMount: function () {
-        console.log('componentWillMount');
     },
     componentDidMount: function () {
         var query = this.props.location.query;
-        console.log('componentDidMount');
-        // Fetch info from Github then update from state
-    },
-    compontWillReceiveProps: function() {
-        console.log('compontWillReceiveProps');
-    },
-    componentWillUnmount: function () {
-        console.log('componentWillUnmount');
+        githubHelpers
+            .getPlayersInfo([query.playerOne, query.playerTwo])
+            .then(function (players) {
+                console.log('PLAYERS', players);
+                this.setState({
+                    isLoading: false,
+                    playersInfo: [players[0], players[1]]
+                });
+            }.bind(this));
     },
     render: function() {
         return (
             <ConfirmBattle isLoading={this.state.isLoading}
-                playerInfo={this.state.playerInfo}/>
+                playersInfo={this.state.playersInfo}/>
         );
     }
 });
